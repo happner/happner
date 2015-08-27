91fe1074e9a9e3c6b135899978eab1c3bf9ea860
Simon Bishop <simonbishop@Simons-MacBook-Pro.local>
2015-08-27 14:32:29 +0200

    working on the system component

---
+++ test/lib/17-system-component.js
@@ -0,0 +1,33 @@
+var Mesh = require('../../lib/system/mesh');
+
+var config = {
+  name: 'system17',
+  dataLayer: {
+    port: 3001,
+    authTokenSecret: 'a256a2fd43bf441483c5177fc85fd9d3',
+    systemSecret: 'mesh',
+    log_level: 'info|error|warning'
+  },
+  modules: {
+    'stress-module': {
+      path: __dirname + '/17-system-stress-module'
+    }
+  },
+  components: {
+    'stress-module': {
+    }
+  }
+}
+
+Mesh().initialize(config, function(err) {
+
+  if (err) {
+    console.log(err);
+    process.exit(err.code || 1);
+  }
+
+  console.log('go to http://localhost:3001/system17/system/app/dashboard.html');
+  console.log('READY');
+  
+
+});
---
+++ test/lib/17-system-stress-module.js
@@ -0,0 +1,12 @@
+module.exports = StressModule;
+
+function StressModule() {}
+
+StressModule.prototype.start = function() {
+  // console.log('start', this.stop);
+}
+
+StressModule.prototype.stop = function() {
+  // console.log('stop', this.$happn.config);
+}
+
