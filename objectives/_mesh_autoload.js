d472a32b8289a4f74647117f613d46e09e614c43
nomilous <nomilous@gmail.com>
2015-09-28 18:07:01 +0200

    jiggle format for happner.js

--- objectives/_mesh_autoload.js
+++ objectives/_mesh_autoload.js
@@ -1,31 +1,76 @@
 module.exports = function() {
 
-  before(function() {
-    delete process.env.SKIP_AUTO_LOAD;
-  });
+  context('autoload enabled', function() {
 
-  after(function() {
-    process.env.SKIP_AUTO_LOAD = '1';
-  });
-
-  it('loads happner.js where found in node_modules', function(done, fs, Mesh, path, expect) {
-
-    var required = [];
-
-    fs.spy(function readFileSync() {
-      var file = arguments[0];
-      if (file.match(/\/happner.js/)) {
-        required.push(path.relative(process.cwd(), file));
-      }
+    before(function() {
+      delete process.env.SKIP_AUTO_LOAD;
     });
 
-    Mesh.create({port: 52384}).then(function() {
-      expect(required).to.eql([
-        'node_modules/happner-dashboard/happner.js',
-        'node_modules/happner-resources/happner.js'
-      ]);
-      done();
-    }).catch(done);
+    after(function() {
+      process.env.SKIP_AUTO_LOAD = '1';
+    });
+
+
+    it('loads happner.js where found in node_modules',
+
+      function(done, fs, Mesh, path, expect) {
+
+        var required = [];
+
+        fs.spy(function readFileSync() {
+          var file = arguments[0];
+          if (file.match(/\/happner.js/)) {
+            required.push(path.relative(process.cwd(), file));
+          }
+        });
+
+        Mesh.create({port: 52384}).then(function() {
+          expect(required).to.eql([
+            'node_modules/happner-dashboard/happner.js',
+            'node_modules/happner-resources/happner.js'
+          ]);
+          done();
+        }).catch(done);
+
+      }
+    );
+
+    it('does not load the module happner.js contains no configs.autoload');
+
+
+    it('loads modules in the mesh where happner.js existed with autoload config present',
+
+      function(done, Mesh, expect) {
+
+        Mesh.create({port: 52385}).then(function(mesh) {
+
+          expect(mesh._mesh.elements['happner-dashboard']).to.exist;
+          expect(mesh._mesh.elements['happner-resources']).to.exist;
+
+          done();
+
+        }).catch(done);
+      }
+    );
+
+    it('allows autoloading from alternative config name and only loads modules whose happner.js define that config name');
+
+    context('suites', function() {
+
+      xit('arrays')
+
+    });
+
+  });
+
+
+  context('autoload disabled', function() {
+
+    it('does not auto load modules containing happner.js');
+
+    it('uses autoload as default config if mesh config specifies module');
+
+    it('can specifiy alternative config name (ie. other than "autoload")');
 
   });
   
