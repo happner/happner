7004e9f113ab62eef0403c19417d9f617914d608
JohanvdWest <jvdwest@gmail.com>
2015-10-14 11:22:10 +0200

    bug/#49 - Unit test that shows config that worked on 0.0.27.

---
+++ test/10-web-middleware.js
@@ -0,0 +1,71 @@
+/**
+ * Created by Johan on 10/14/2015.
+ */
+
+// Uses unit test 2 modules
+var should = require('chai').should();
+var Mesh = require('../');
+var http = require('http');
+
+
+describe('tests that we can add middleware before a static', function (done) {
+
+  this.timeout(20000);
+
+  var defaultTimeout = (process.arch == 'arm') ? 50000 : 10000;
+
+  var sep = require('path').sep;
+  var libFolder = __dirname + sep + 'lib' + sep;
+
+  var config = {
+    name: "middlewareMesh",
+    dataLayer: {
+      authTokenSecret: 'a256a2fd43bf441483c5177fc85fd9d3',
+      systemSecret: 'mesh',
+      log_level: 'error',
+      port:10000
+      //setOptions:{}
+    },
+    endpoints: {},
+    modules: {
+      "middlewareTest": {
+        path: libFolder + "10-middleware"
+      }
+    },
+    components: {
+      "www": {
+        moduleName: "middlewareTest",
+        // scope: "component",//either component(mesh aware) or module - default is module
+        schema: {
+          "exclusive": false,//means we dont dynamically share anything else
+          "methods": {}
+        },
+        web: {
+          routes: {
+            "static": ["checkIndex","static"]
+          }
+        }
+      }
+    }
+  };
+
+  var mesh;
+
+  before(function (done) {
+    this.timeout(defaultTimeout);
+    mesh = new Mesh();
+    mesh.initialize(config, function (err) {
+      done();
+    });
+  });
+
+
+  it('can get index.html that middleware renames to index.htm', function (done) {
+    this.timeout(defaultTimeout);
+    http.get('http://localhost:10000/index.html', function(resp) {
+      resp.statusCode.should.eql(200);
+      done();
+    })
+  });
+
+});
