2fdb43e312d3d562cb5cc91efc6ce55a9a27b807
Simon Bishop <simonbishop@Simons-MacBook-Pro.local>
2016-01-08 19:50:00 +0200

    token being added to client, busy with positive test

--- test/c7-permissions-web.js
+++ test/c7-permissions-web.js
@@ -6,6 +6,7 @@
 var should = require('chai').should();
 var Mesh = require('../');
 var http = require('http');
+var test_id = require('shortid').generate();
 
 
 describe('c7-permissions-web', function (done) {
@@ -21,7 +22,8 @@ describe('c7-permissions-web', function (done) {
     name: "middlewareMesh",
     datalayer: {
       secure:true,
-      port:10000
+      port:10000,
+      adminPassword: test_id
     },
     modules: {
       "middlewareTest": {
@@ -60,12 +62,31 @@ describe('c7-permissions-web', function (done) {
     this.mesh.stop(done);
   })
 
-  it('can get index.html that middleware renames to index.htm', function (done) {
+  it('fails to access a file, missing the token', function (done) {
     this.timeout(defaultTimeout);
     http.get('http://localhost:10000/index.html', function(resp) {
       resp.statusCode.should.eql(403);
       done();
     })
+  });
+
+  var adminClient = new Mesh.MeshClient({secure:true, port:10000});
+
+  it('logs in wth the admin user, we have a token', function (done) {
+    
+    var credentials = {
+      username: '_ADMIN', // pending
+      password: test_id
+    }
+
+    adminClient.login(credentials).then(function(){
+      
+
+      console.log('session token is:::', adminClient.token);
+
+
+    }).catch(done);
+
   });
 
 });
