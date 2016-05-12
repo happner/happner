a8b2bcb9fcc70884b088fadce8cdfe15f193a4f2
Simon Bishop <simonbishop@Simons-MacBook-Pro.local>
2016-05-12 20:04:44 +0200

    redirect manually tested

--- test/d6-startup-proxy.js
+++ test/d6-startup-proxy.js
@@ -84,9 +84,39 @@ describe('d6-startup-proxy', function (done) {
 
   var proxyManager;
 
-  it('starts the proxy server using the proxy manager', function (done) {
+  // it('starts the proxy server using the proxy manager, long running', function (done) {
+  //
+  //   this.timeout(60000);
+  //
+  //   var ProxyManager = require('../lib/startup/proxy_manager');
+  //   proxyManager = new ProxyManager();
+  //
+  //   proxyManager.start({port: 55000}, function (e) {
+  //
+  //     if (e) return done(e);
+  //
+  //     proxyManager.progress('test', 10);
+  //     proxyManager.progress('test1', 20);
+  //
+  //     doRequest('/progress', null, null, function(data){
+  //
+  //       var prog_data = JSON.parse(data);
+  //
+  //       expect(prog_data[0].log).to.be('test');
+  //       expect(prog_data[0].percentComplete).to.be(10);
+  //       expect(prog_data[1].log).to.be('test1');
+  //       expect(prog_data[1].percentComplete).to.be(20);
+  //
+  //       setTimeout(done, 40000);
+  //
+  //
+  //     }, 55000);
+  //
+  //   })
+  //
+  // });
 
-    this.timeout(60000);
+  it('starts the proxy server using the proxy manager', function (done) {
 
     var ProxyManager = require('../lib/startup/proxy_manager');
     proxyManager = new ProxyManager();
@@ -107,7 +137,7 @@ describe('d6-startup-proxy', function (done) {
         expect(prog_data[1].log).to.be('test1');
         expect(prog_data[1].percentComplete).to.be(20);
 
-        setTimeout(done, 40000);
+        done();
 
 
       }, 55000);
