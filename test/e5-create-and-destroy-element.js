0632107e7d13eb6fbd72c058e30a2a067f9b7b1e
nomilous <nomilous@gmail.com>
2016-07-28 18:52:45 +0200

    can add a new component to the mesh (from another component using _mesh)

--- test/e5-create-and-destroy-element.js
+++ test/e5-create-and-destroy-element.js
@@ -6,13 +6,48 @@ var path = require('path');
 var filename = path.basename(__filename);
 var should = require('chai').should();
 var Happner = require('../');
+var Promise = require('bluebird');
+var request = Promise.promisify(require('request'));
 
 describe(filename, function () {
+
+  require('benchmarket').start();
+  after(require('benchmarket').store());
 
   var mesh;
 
   before(function (done) {
-    Happner.create({})
+    Happner.create({
+        modules: {
+          'factory': { // component that adds another component via _mesh
+            instance: {
+              createComponent: function ($happn, name, callback) {
+                $happn._mesh._createElement({
+                  module: {
+                    name: name,
+                    config: {
+                      instance: {
+                        method: function (callback) {
+                          callback(null, name + ' OK');
+                        }
+                      }
+                    }
+                  },
+                  component: {
+                    name: name,
+                    config: {}
+                  }
+                }, callback);
+              }
+            }
+          }
+        },
+        components: {
+          'factory': {
+            accessLevel: 'mesh'
+          }
+        }
+      })
       .then(function (_mesh) {
         mesh = _mesh;
         done();
@@ -25,27 +60,36 @@ describe(filename, function () {
     mesh.stop({reconnect: false}, done);
   });
 
-  it('can add a component to the mesh', function (done) {
 
+  it('can add a component to the mesh', function (done) {
     mesh._createElement({
-      module: {
-        name: 'newComponent',
-        config: {
-          instance: {
-            method: function(callback) {
-              callback(null, 'newComponent OK');
+        module: {
+          name: 'newComponent',
+          config: {
+            instance: {
+              method: function (callback) {
+                callback(null, 'newComponent OK');
+              },
+              page: function (req, res) {
+                res.end('WEB PAGE');
+              }
+            }
+          }
+        },
+        component: {
+          name: 'newComponent',
+          config: {
+            web: {
+              routes: {
+                page: 'page'
+              }
             }
           }
         }
-      },
-      component: {
-        name: 'newComponent',
-        config: {}
-      }
-    })
+      })
 
       .then(function () {
-        // use the new component
+        // use the new component's method
         return mesh.exchange.newComponent.method();
       })
 
@@ -53,19 +97,48 @@ describe(filename, function () {
         result.should.equal('newComponent OK');
       })
 
+      .then(function () {
+        // use new component's web method
+        return request('http://localhost:55000/newComponent/page');
+      })
+
+      .then(function (result) {
+        result[1].should.equal('WEB PAGE');
+      })
+
       .then(done)
       .catch(done);
   });
 
 
-  xit('can add a new component to the mesh (from another component via _mesh)', function (done) {
+  it('can add a new component to the mesh (from another component using _mesh)', function (done) {
+    mesh.exchange.factory.createComponent('componentName')
+
+      .then(function () {
+        // use the new component
+        return mesh.exchange.componentName.method();
+      })
+
+      .then(function (result) {
+        result.should.equal('componentName OK');
+      })
+
+      .then(done)
+      .catch(done);
+  });
+
+
+  xit('can remove components from the mesh including web methods', function () {
 
 
   });
 
-  xit('can remove components from the mesh', function () {
+  it('emits description change on adding component');
 
+  it('emits description change on destroying component');
 
-  });
+  it('clears messenger subscriptions on remove component');
+
+  require('benchmarket').stop();
 
 });
