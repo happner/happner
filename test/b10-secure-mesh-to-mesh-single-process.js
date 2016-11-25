df49de8f171eb0f2069c533e8aae3759761ffe0d
Jens Eggers <eggers.jens@gmail.com>
2016-11-25 10:30:35 +0200

    issue-247 - add b10-secure-mesh-to-mesh-single-process.js

---
+++ test/b10-secure-mesh-to-mesh-single-process.js
@@ -0,0 +1,102 @@
+var path = require('path');
+var remoteConfig = require('./lib/b2-first-mesh-config');
+var assert = require('assert');
+var Mesh = require('..');
+
+describe(path.basename(__filename), function () {
+
+  require('benchmarket').start();
+  after(require('benchmarket').store());
+
+  context('on remote mesh', function () {
+
+    var remoteMesh;
+    var config = {
+      name: 'mesh2',
+      dataLayer: {
+        secure: true,
+        port: 51233
+      },
+      endpoints: {
+        remoteMesh: {  // remote mesh node
+          config: {
+            port: 51234,
+            username: '_ADMIN',
+            password: 'testb2' // TODO This was necessary, did not default
+          }
+        }
+      },
+      modules: {},
+      components: {}
+    };
+
+    this.timeout(120000);
+
+    before(function (done) {
+
+      Mesh
+        .create(remoteConfig)
+        .then(function (createdMesh) {
+          remoteMesh = createdMesh;
+        })
+        .then(function () {
+          return Mesh.create(config);
+        })
+        .then(function (createdMesh) {
+          mesh = createdMesh;
+        })
+        .then(function () {
+          done();
+        })
+        .catch(done)
+    });
+
+
+    after(function (done) {
+      remoteMesh.stop({reconnect: false})
+        .then(function () {
+          return mesh.stop({reconnect: false})
+        })
+        .then(function () {
+          done();
+        })
+        .catch(done);
+    });
+
+    it("can call remote component function", function (done) {
+
+      mesh.event.remoteMesh.remoteComponent.on(
+        'whoops',
+        function handler(data) {
+          console.log(data);
+          done();
+        },
+        function callback(err) {
+          if (err) done(err);
+        }
+      );
+
+      mesh.exchange.remoteMesh.remoteComponent.remoteFunction(
+        'one!', 'two!', 'three!', function (err, res) {
+
+          assert(res == 'one! two! three!, wheeeeeeeeeeeeheeee!');
+
+        });
+    });
+
+    it('we know when there was an accident', function (done) {
+
+      mesh.exchange.remoteMesh.remoteComponent.causeError(function (err, res) {
+
+        assert(err.toString().match(/ErrorType: Error string/));
+        done();
+
+      });
+
+    });
+
+  });
+
+  require('benchmarket').stop();
+
+});
---
+++ test/lib/b2-first-mesh-config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  name: 'remoteMesh',
+  dataLayer: {
+    secure: true,
+    port: 51234,
+    authTokenSecret: 'a256a2fd43bf441483c5177fc85fd9d3',
+    systemSecret: 'mesh',
+    adminPassword: 'testb2'
+  },
+  endpoints: {},
+  modules: {
+    "remoteComponent": {
+      path: __dirname + "/4-remote-component",
+      constructor: {
+        type: "sync",
+        parameters: []
+      }
+    }
+  },
+  components: {
+    "remoteComponent": {
+      moduleName: "remoteComponent",
+      schema: {
+        "exclusive": false,
+        "methods": {
+          "remoteFunction": {
+            parameters: [
+              {name: 'one', required: true},
+              {name: 'two', required: true},
+              {name: 'three', required: true},
+              {name: 'callback', type: 'callback', required: true}
+            ]
+          }
+          ,
+          "causeError": {
+            parameters: [
+              {name: 'callback', type: 'callback', required: true}
+            ]
+          }
+        }
+      }
+    }
+  }
+};
