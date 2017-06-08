0f83ab17da5e83b3ae51c05297814250283e5d57
nomilous <nomilous@gmail.com>
2017-06-08 14:31:25 +0200

    added ping and pong config to Endpoint

--- test/g1-start-with-primus-opts.js
+++ test/g1-start-with-primus-opts.js
@@ -4,11 +4,13 @@ var Happner = require('..');
 
 describe(filename, function () {
 
-  var server;
+  var server1;
+  var server2;
 
-  before('start server', function (done) {
+  before('start server 1', function (done) {
 
     Happner.create({
+        name: 'SERVER1',
         datalayer: {
           pubsub: {
             primusOpts: {
@@ -20,7 +22,7 @@ describe(filename, function () {
       })
 
       .then(function (_server) {
-        server = _server;
+        server1 = _server;
         done();
       })
 
@@ -28,24 +30,81 @@ describe(filename, function () {
 
   });
 
-  after('stop server', function (done) {
+  before('start server 2', function (done) {
 
-    if (!server) return done();
-    server.stop({
+    Happner.create({
+        name: 'SERVER2',
+        endpoints: {
+          'SERVER1': {
+            config: {
+              host: 'localhost',
+              port: 55000,
+              pubsub: {
+                options: {
+                  ping: 5000,
+                  pong: 1000
+                }
+              }
+            }
+          }
+        },
+        datalayer: {
+          port: 55001,
+          pubsub: {
+            primusOpts: {
+              allowSkippedHeartBeats: 20,
+              pongSkipTime: 500
+            }
+          }
+        }
+      })
+
+      .then(function (_server) {
+        server2 = _server;
+        done();
+      })
+
+      .catch(done);
+
+  });
+
+  after('stop server 2', function (done) {
+
+    if (!server2) return done();
+    server2.stop({
       reconnect: false
     }, done);
 
   });
 
-  it('sets primus opts', function (done) {
+  after('stop server 1', function (done) {
 
-    var primusOptions = server._mesh.datalayer.server.services
+    if (!server1) return done();
+    server1.stop({
+      rec1nnect: false
+    }, done);
+
+  });
+
+  it('sets server primus opts', function (done) {
+
+    var serverPrimusOptions = server1._mesh.datalayer.server.services
       .pubsub.primus.options;
 
-    expect(primusOptions.allowSkippedHeartBeats).to.equal(20);
-    expect(primusOptions.pongSkipTime).to.equal(500);
+    expect(serverPrimusOptions.allowSkippedHeartBeats).to.equal(20);
+    expect(serverPrimusOptions.pongSkipTime).to.equal(500);
     done();
 
   });
+
+  it('sets endpoint primus opts', function (done) {
+
+    var endpointPrimusOpts = server2._mesh.endpoints.SERVER1.data.pubsub.options;
+
+    expect(endpointPrimusOpts.ping).to.equal(5000);
+    expect(endpointPrimusOpts.pong).to.equal(1000);
+    done();
+
+  })
 
 });
